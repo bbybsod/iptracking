cc88e89e9e1d1a5310de02984f2eed01f21fc750
sean myxzu <145573157+bbybsod@users.noreply.github.com>
2025-01-30 04:23:40 -0800

    Add files via upload

---
+++ iptrack.py
@@ -0,0 +1,32 @@
+import requests
+
+def get_ip_geolocation(ip_address):
+    
+    url = f"https://ipinfo.io/{ip_address}/json"
+    response = requests.get(url)
+    
+    if response.status_code == 200:
+        data = response.json()
+        return data
+    else:
+        return None
+
+def main():
+    ip_address = input("Masukkan alamat IP: ")
+    geolocation_data = get_ip_geolocation(ip_address)
+
+    if geolocation_data:
+        print("\nInformasi Geolokasi:")
+        print(f"IP: {geolocation_data.get('ip')}")
+        print(f"Kota: {geolocation_data.get('city')}")
+        print(f"Wilayah: {geolocation_data.get('region')}")
+        print(f"Negara: {geolocation_data.get('country')}")
+        print(f"Lokasi (Lat, Lon): {geolocation_data.get('loc')}")
+        print(f"Provider Internet (ISP): {geolocation_data.get('org')}")
+        print(f"Kode Pos: {geolocation_data.get('postal')}")
+        print(f"Zona Waktu: {geolocation_data.get('timezone')}")
+    else:
+        print("Gagal mendapatkan informasi geolokasi.")
+
+if __name__ == "__main__":
+    main()
